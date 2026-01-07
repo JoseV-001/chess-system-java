2d7a4a2972fc005299d86e5b3c34c55aafa2e3b1
JoseV-001 <Leaphaz@gmail.com>
2026-01-06 22:43:25 -0300

    Moving pieces

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,31 @@
 package application;
 
 import chess.ChessMetch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import javax.script.ScriptContext;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMetch chessMetch = new ChessMetch();
-        UI.printBoard(chessMetch.getPieces());
+
+        while (true){
+            UI.printBoard(chessMetch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMetch.performChessMove(source, target);
+        }
+
 
     }
 }
